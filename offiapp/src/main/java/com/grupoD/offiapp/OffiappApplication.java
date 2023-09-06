ccfd7f71d0094f7e142cb201c1a4b87b0d6ff63e
Gonzalo <gonzaloalvarez3030@gmail.com>
2023-09-06 14:05:31 -0300

    carga de proyecto general

---
+++ offiapp/src/main/java/com/grupoD/offiapp/OffiappApplication.java
@@ -0,0 +1,13 @@
+package com.grupoD.offiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffiappApplication.class, args);
+	}
+
+}
