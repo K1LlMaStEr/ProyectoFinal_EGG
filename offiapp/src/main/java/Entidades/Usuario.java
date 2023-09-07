5a26439e4c601ad72d59d423868219b74840ca7d
Gonzalo <gonzaloalvarez3030@gmail.com>
2023-09-07 19:53:08 -0300

    entidad usuario y servidor listas

--- offiapp/src/main/java/Entidades/Usuario.java
+++ offiapp/src/main/java/Entidades/Usuario.java
@@ -5,10 +5,97 @@
  */
 package Entidades;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  *
  * @author pulaf
  */
+@Entity
 public class Usuario {
+     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid", strategy="uuid2") 
+    private String id;
+     private String nombre;
+     private String contrasenia;
+     private String email;
+     
+      @Enumerated(EnumType.STRING)
+     private String Rol;
+     
+      @Temporal(TemporalType.DATE)
+    private Date fecha;
+
+    public Usuario() {
+    }
+
+    public Usuario(String id, String nombre, String contrasenia, String email, String Rol, Date fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contrasenia = contrasenia;
+        this.email = email;
+        this.Rol = Rol;
+        this.fecha = fecha;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRol() {
+        return Rol;
+    }
+
+    public void setRol(String Rol) {
+        this.Rol = Rol;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+      
+      
+     
     
 }
