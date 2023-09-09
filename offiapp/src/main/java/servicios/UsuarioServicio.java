1f6a7b793b8bce21edd0778b4fc236686240840a
Paula Fryd <frydpaula@gmail.com>
2023-09-09 16:35:34 -0300

    Solucione el error de seguridad

--- offiapp/src/main/java/servicios/UsuarioServicio.java
+++ offiapp/src/main/java/servicios/UsuarioServicio.java
@@ -7,19 +7,34 @@ package servicios;
 import Entidades.Usuario;
 import enumeraciones.Rol;
 import excepciones.MiException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import repositorios.UsuarioRepositorio;
+
 
 /**
  *
  * @author gonza
  */
-public class UsuarioServicio {
-    @Autowired
+@Service
+public class UsuarioServicio implements UserDetailsService {
+  @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
+
+
     @Transactional
     public void registrar(String nombre, String email, String password, String password2) throws MiException{
         validar(nombre, email, password, password2);
@@ -48,5 +63,20 @@ public class UsuarioServicio {
      }
         
     }
+     
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario= usuarioRepositorio.buscarPorEmail(email);
+        if(usuario!=null){
+            List<GrantedAuthority> permisos= new ArrayList();
+            GrantedAuthority p= new SimpleGrantedAuthority("ROLE_"+ usuario.getRol().toString());
+            permisos.add(p);
+            return new User(usuario.getEmail(),usuario.getPassword(),permisos);
+        
+    }else{
+            return null;
+        }
+       
+    }
     
-}
+    }
