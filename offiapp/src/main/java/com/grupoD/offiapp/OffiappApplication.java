e7fc5a43640f118ea2a708eec1670cc454820816
Paula Fryd <frydpaula@gmail.com>
2023-09-12 14:12:33 -0300

    Empezando los controllers

--- offiapp/src/main/java/com/grupoD/offiapp/OffiappApplication.java
+++ offiapp/src/main/java/com/grupoD/offiapp/OffiappApplication.java
@@ -2,8 +2,10 @@ package com.grupoD.offiapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "sources.servicios")
 public class OffiappApplication {
 
 	public static void main(String[] args) {
---
+++ offiapp/src/main/java/com/grupoD/offiapp/UsuarioServicio.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupoD.offiapp;
+
+import Entidades.Usuario;
+import enumeraciones.Rol;
+import excepciones.MiException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import repositorios.UsuarioRepositorio;
+
+
+/**
+ *
+ * @author gonza
+ */
+@Service
+public class UsuarioServicio implements UserDetailsService {
+  @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+
+    @Transactional
+    public void registrar(String nombre, String email, String password, String password2) throws MiException{
+        validar(nombre, email, password, password2);
+        Usuario usuario= new Usuario();
+        usuario.setNombreUser(nombre);
+        usuario.setEmail(email);
+        usuario.setContrasenia(new BCryptPasswordEncoder().encode(password));
+        usuario.setRol(Rol.USER);
+        usuarioRepositorio.save(usuario);
+        
+        
+    }
+     public void validar(String nombre, String email, String password, String password2) throws MiException  {
+        
+         if(nombre.isEmpty()||nombre==null){
+        throw new MiException("el nombre no puede estar vacio");
+    }    
+    if(email.isEmpty()||email==null){
+        throw new MiException("el email no puede estar vacio");
+    }
+     if(password.isEmpty()||password==null|| password.length()<=5){
+        throw new MiException("el password no puede estar vacio y tener menos de 5 digitos");
+    }    
+     if(!password.equals(password2)){
+          throw new MiException("los passwords no pueden ser distintos");
+     }
+        
+    }
+     
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario= usuarioRepositorio.buscarPorEmail(email);
+        if(usuario!=null){
+            List<GrantedAuthority> permisos= new ArrayList();
+            GrantedAuthority p= new SimpleGrantedAuthority("ROLE_"+ usuario.getRol().toString());
+            permisos.add(p);
+            return new User(usuario.getEmail(),usuario.getContrasenia(),permisos);
+        
+    }else{
+            return null;
+        }
+       
+    }
+    
+    }
