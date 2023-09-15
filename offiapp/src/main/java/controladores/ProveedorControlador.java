470f2b4dc4ad44627387ea73479da89993de78bf
Paula Fryd <frydpaula@gmail.com>
2023-09-15 12:51:47 -0300

    excepciones en controladores

--- offiapp/src/main/java/controladores/ProveedorControlador.java
+++ offiapp/src/main/java/controladores/ProveedorControlador.java
@@ -6,11 +6,10 @@
 package controladores;
 
 
-import enumeraciones.Rol;
 import excepciones.MiException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,35 +17,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import servicios.UsuarioServicio;
+import servicios.ProveedorServicio;
 
 /**
  *
  * @author pulaf
  */
-  @Controller
-@RequestMapping ("/usuario")
-public class UsuarioControlador {
-  
-@Autowired
-private UsuarioServicio usuarioServicio;
 
+    @Controller
+@RequestMapping ("/provedor")
+public class ProveedorControlador {
+        @Autowired 
+        private ProveedorServicio proveedorServicio;
     @GetMapping ("/registrar")
     public String Registrar(){
-        return "registro_usuario.html";
+        return "registro_provedor.html";
     }
     @PostMapping ("/registro")
-    public String Registro(@RequestParam String nombreUser,@RequestParam (required= false)int idUser,
-    @RequestParam String direccion,@RequestParam String email,@RequestParam String contrasenia,
-    @RequestParam (required=false) Rol rol, ModelMap modelo) throws MiException {
-     try{
-        usuarioServicio.registrar(nombreUser, email, contrasenia, contrasenia);
-        modelo.put("exito", "Usted se ha registrado correctamente");
+    public String Registro(@RequestParam String id, @RequestParam String nombreProv, @RequestParam String contrasenia,@RequestParam
+            int telefono,@RequestParam String servicio, @RequestParam int precioHora,
+            @RequestParam (required=false)String descripcion,@RequestParam String email,ModelMap modelo){
+      
+      try{
+        proveedorServicio.crearProveedor( nombreProv,  descripcion, telefono, servicio);
+        modelo.put("exito", "Usted se registro correctamente");
      }catch(MiException ex){
-        modelo.put("error", ex.getMessage());
+          modelo.put("error", ex.getMessage());
          return "registro_usuario.html";
      }
         return "index.html";
     }
-}
-
+    }
